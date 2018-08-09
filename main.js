a2519473ff283f774429ac6c6ce14b8e7712730d
Nolan Clark <nclark@Davids-Mac-mini.local>
2018-08-09 01:06:55 -0500

    Added navigation

---
+++ main.js
@@ -0,0 +1,37 @@
+function getAllNavLinks() {
+    var navContainer = document.getElementsByTagName("nav")[0];
+    return navContainer.getElementsByTagName("a");
+}
+
+function navigateToContent(event) {
+    event.preventDefault();
+    var clickedLink = event.target;
+    var sections = document.getElementsByTagName("section");
+    var navRef = clickedLink.getAttribute("href");
+    var contentId = navRef.substring(1);
+    for(var section = 0; section < sections.length; section++) {
+        var contentSection = sections[section];
+        if(contentSection.id == contentId) {
+            contentSection.classList.add("selected-content");
+        } else {
+            contentSection.classList.remove("selected-content");
+        }
+    }
+    var navLinks = getAllNavLinks();
+    for(var link = 0; link < navLinks.length; link++) {
+        var navLink = navLinks[link];
+        if(navLink.getAttribute("href") == navRef) {
+            navLink.classList.add("selected-tab");
+        } else {
+            navLink.classList.remove("selected-tab");
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    var navLinks = getAllNavLinks();
+    for(var link = 0; link < navLinks.length; link++) {
+        var navLink = navLinks[link];
+        navLink.addEventListener("click", navigateToContent);
+    }
+});
